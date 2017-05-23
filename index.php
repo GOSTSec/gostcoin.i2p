27c2abaddc6c64082e7e69818bd501d1100d667e
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-05-23 17:46:03 +0700

    .

--- index.php
+++ index.php
@@ -3,11 +3,7 @@ require_once("config.php");
 include("class/language.php");
 include("class/pages.php");
 
-if(isset($_COOKIE['lang']))
- $pages = new pages($_COOKIE['lang']);
-else
-  $pages = new pages( locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']) );
-  
+$pages = new pages(); 
 $pages->GetPage("header");
 
 if(!isset($_GET['page']))
