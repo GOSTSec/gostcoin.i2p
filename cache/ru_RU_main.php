b3609dcc4a36326d82dc1e3322902642f6ab2ef5
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-05-28 18:16:31 +0700

    Multilagual PHP version
    by Plaz

---
+++ cache/ru_RU_main.php
@@ -0,0 +1,20 @@
+<body>
+		<span class=logo></span>
+		<h1 class="red">Криптовалюта использующая ГОСТ Р 34.10-2012, которая, работает полностью через сеть I2P</h1>
+		Глобальный-интернет:
+		<br>
+		<ul>
+			<li><a href="https://github.com/GOSTSec/gostcoin">Кошелек</a></li>
+			<li><a href="https://github.com/GOSTSec/cpuminer-x11-gost">CPU майнер</a></li>
+			<li><a href="https://github.com/GOSTSec/ccminer">GPU майнер</a></li>
+		</ul>
+		<br>
+		I2P:
+		<br>
+		<ul>
+			<li><a href="http://git.psi.i2p/GOSTSec/gostcoin">Кошелек</a></li>
+			<li><a href="http://git.psi.i2p/GOSTSec/cpuminer-x11-gost">CPU майнер</a></li>
+			<li><a href="http://git.psi.i2p/GOSTSec/ccminer">GPU майнер</a></li>
+		</ul>
+</body>
+</html>
