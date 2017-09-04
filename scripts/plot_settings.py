c1d221158d8dcf5941a1f3571bedc2a1b135d896
Trashgen_Genderovich Aka Plazmovich <a.yvarov@yandex.ru>
2017-09-04 23:27:16 +0700

    DPI

--- scripts/plot_settings.py
+++ scripts/plot_settings.py
@@ -1,7 +1,7 @@
 PLOT_TITLE = 'BTC / GST graph'
 Y_AXIS_NAME = 'BTC price'
 IMAGE_EXIST_PATH = '../img/GSTBTC_graph.png'
-DPI = 60 # affects the size of output image
+DPI = 120 # affects the size of output image
 
 NVSPC_HISTORY_API = 'http://nvspc.i2p/api/dummy/gettradelog?e=5&c=500&bt=3'
 
